2f833217a6e36111e74ed6435412159c8d319c4f
Matias Cancina <cancinamatias@gmail.com>
2026-04-20 12:27:55 -0300

    clase 10

---
+++ Cuatri 2/Ejs .NET/Micros/micros.Tests/FiacaTest1.cs
@@ -0,0 +1,39 @@
+﻿using _20260414___Clase_09.Clases;
+using Xunit;
+
+namespace micros.Tests;
+
+public class MicroMock : Micro
+{
+    public MicroMock(int volumen, int cantidadAsientos, int cantidadParados)
+        : base(volumen, cantidadAsientos, cantidadParados)
+    {
+    }
+}
+
+public class UnitTest1
+{
+    [Fact]
+    public void FiacaSubeTest()
+    {
+        var fiaca = new Fiaca();
+        var apurado = new Apurado();
+        var micro = new Micro(200, 2, 5);
+        micro.Subir(apurado);
+
+        Assert.True(fiaca.AceptaSubir(micro));
+    }
+
+    [Fact]
+    public void FiacaNoSubeTest()
+    {
+        var fiaca = new Fiaca();
+        var apurado = new Apurado();
+        var micro = new MicroMock(200, 1, 5);
+
+        micro.Pasajeros.Add(new Apurado());
+
+        Assert.False(fiaca.AceptaSubir(micro));
+    }
+
+}
